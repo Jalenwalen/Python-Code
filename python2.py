8808569be968e2eb3af10ace3c65a8be0458a83f
Dylan M. <120059262+26minatreadt@users.noreply.github.com>
2024-10-16 13:12:01 -0500

    Update python2.py

--- python2.py
+++ python2.py
@@ -93,6 +93,12 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
     return a / b
 
+
+print(add(10 ,5))
+print(subtract(10 ,5))
 print(multiply(10 ,5))
+print(divide(10 ,5))
