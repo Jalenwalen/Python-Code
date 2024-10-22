744a42d78046190142603c650e57646c7c6cae1a
Dylan M. <120059262+26minatreadt@users.noreply.github.com>
2024-10-22 13:07:40 -0500

    added custom_module.py and pythonimports.py

---
+++ pythonimports.py
@@ -0,0 +1,12 @@
+# Using the math and random libraries
+import math
+import random
+
+print(math.sqrt(16))
+print(random.randint(1, 100))
+
+
+# Main File
+import custom_module
+
+print(custom_module.greet("Alice"))
